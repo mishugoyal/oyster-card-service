0b33bd04c5739e3ba48b76c0c272515010f93081
Mishu Goyal <mishu.goyal@thetrainline.com>
2020-10-18 17:50:45 +0530

    Add implementation for swipe in oyster card

--- src/main/java/com/deserve/app/card/OysterCardInMemoryImpl.java
+++ src/main/java/com/deserve/app/card/OysterCardInMemoryImpl.java
@@ -1,13 +1,11 @@
 package com.deserve.app.card;
 
 import com.deserve.app.fare.FareCalculator;
-import com.deserve.app.model.JourneyType;
-import com.deserve.app.model.Station;
-import com.deserve.app.model.Transaction;
-import com.deserve.app.model.TransactionType;
+import com.deserve.app.model.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class OysterCardInMemoryImpl implements OysterCard {
   private double balance;
@@ -22,6 +20,13 @@ public class OysterCardInMemoryImpl implements OysterCard {
 
   @Override
   public boolean swipeIn(Station station, JourneyType journeyType) {
+    Journey journey = getSwipeInJourney(station, journeyType);
+    final Optional<Double> fare = fareCalculator.getFare(journey);
+    if (fare.isPresent() && isSufficientBalance(fare.get())) {
+      getJourneyTransaction(journey, fare.get());
+      reduceBalance(fare.get());
+      return true;
+    }
     return false;
   }
 
@@ -52,5 +57,28 @@ public class OysterCardInMemoryImpl implements OysterCard {
     transaction.setAmount(amount);
     transaction.setTransactionType(TransactionType.CREDIT);
     return transaction;
+  }
+
+  private void getJourneyTransaction(Journey journey, Double fare) {
+    Transaction transaction = new Transaction();
+    transaction.setAmount(fare);
+    transaction.setTransactionType(TransactionType.DEBIT);
+    transaction.setJourney(journey);
+    transactions.add(transaction);
+  }
+
+  private void reduceBalance(Double amount) {
+    balance -= amount;
+  }
+
+  private boolean isSufficientBalance(Double fare) {
+    return fare <= this.balance;
+  }
+
+  private Journey getSwipeInJourney(Station station, JourneyType journeyType) {
+    Journey journey = new Journey();
+    journey.setSource(station);
+    journey.setJourneyType(journeyType);
+    return journey;
   }
 }
