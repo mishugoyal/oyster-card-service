d8ccc2a217020f1d43e51af43859a7b82370e307
Mishu Goyal <mishu.goyal@thetrainline.com>
2020-10-18 16:30:11 +0530

    Add ToString annotation for Journey and Transaction data model

--- src/main/java/com/deserve/app/model/Journey.java
+++ src/main/java/com/deserve/app/model/Journey.java
@@ -2,11 +2,13 @@ package com.deserve.app.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Optional;
 
 @Getter
 @Setter
+@ToString
 public class Journey {
   private Station source;
   private Station destination = null;
--- src/main/java/com/deserve/app/model/Transaction.java
+++ src/main/java/com/deserve/app/model/Transaction.java
@@ -2,11 +2,13 @@ package com.deserve.app.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Optional;
 
 @Getter
 @Setter
+@ToString
 public class Transaction {
   private TransactionType transactionType;
   private double amount;
